8052e88b39e519da0930192b676a18f261f6ded4
Paige Wiener <snowplushie@yahoo.com>
2019-04-15 22:55:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,5 +50,8 @@ class Song
   def self.new_from_filename(filename)
     parts = filename.split(" - ")
     artist_name, song_name, genre_name = parts[0], parts[1], parts[2].gsub(".mp3", "")
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
   end
 end
