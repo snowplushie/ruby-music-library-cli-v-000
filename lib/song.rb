4673dd6fcd2a7f0f4e92bee8afdfc368eac41e1a
Paige Wiener <snowplushie@yahoo.com>
2019-04-15 22:35:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,5 +36,11 @@ class Song
     song
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    all.detect {|s| s.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
 end
